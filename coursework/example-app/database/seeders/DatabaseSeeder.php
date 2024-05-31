72f86037a9bbb58700fe2fe702370f3e3da9edba
kristinatibeikina <kristina.tibeykina04@mail.ru>
2024-05-31 23:22:52 +0700

    сидер на админа

--- coursework/example-app/database/seeders/DatabaseSeeder.php
+++ coursework/example-app/database/seeders/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\User;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,11 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         //\App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         User::factory()->create([
+            'name' => 'admin',
+             'email' => 'admin@example.com',
+             'password'=>'adminadmin',
+             'id_role'=>'1'
+         ]);
     }
 }
